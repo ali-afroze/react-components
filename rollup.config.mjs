a35dc4f8ef09bfbab929ba33feb5617a8621a17d
ali-afroze <zubae@live.in>
2025-01-23 21:00:57 +0000

    feat: :sparkles: bundled build successful
    - Made sure `esm` and `cjs` bundles separately instead of single run.
    - Added extensive `tsconfig` rules

---
+++ rollup.config.mjs
@@ -0,0 +1,63 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/esm",
+      format: "esm",
+      sourcemap: true
+    },
+    external: ["react", "react-dom", "react/jsx-runtime"],
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss({
+        extensions: [".css"],
+        inject: true, 
+        extract: false
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        outDir: "dist/esm",
+        declaration: true,
+        declarationDir: "dist/esm/types",
+        exclude: ["**/*.test.ts", "**/*.test.tsx", "**/*.stories.ts", "**/*.stories.tsx"],
+      })
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/cjs",      
+      format: "cjs",
+      sourcemap: true
+    },
+    external: ["react", "react-dom", "react/jsx-runtime"],
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss({
+        extensions: [".css"],
+        inject: true,
+        extract: false
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: false,
+        outDir: "dist/cjs",
+        exclude: ["**/*.test.ts", "**/*.test.tsx", "**/*.stories.ts", "**/*.stories.tsx"],
+      })
+    ]
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
+];
